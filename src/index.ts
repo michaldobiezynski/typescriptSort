bf21a50455f45c342c1f05b2283269e1ee956ecd
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-07-08 09:01:52 +0100

    sorting arbitrary collections

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollections } from "./CharactersCollections";
 
 const numberCollection = new NumbersCollection([10, 30, -5, 0]);
 
 const sorter = new Sorter(numberCollection);
 sorter.sort();
 console.log(numberCollection.data);
+
+const charactersCollection = new CharactersCollections("Xaayb");
+
+const sorter2 = new Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection.data);
