2f1fa8b6984807bfd41e8dc6c932146a74014565
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-07-08 08:51:22 +0100

    interface definition

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 
-const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+const numberCollection = new NumbersCollection([10, 30, -5, 0]);
 
 const sorter = new Sorter(numberCollection);
 sorter.sort();
